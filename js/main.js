5e424a9d03f233da4b811cf5e1f793330a1326ac
Asaduzzaman <AsadRifat@users.noreply.github.com>
2018-09-03 12:02:01 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 
+function($){
+  "use strict";
+  
+  jQuery(document).ready(function(){
+    $('h2').on('click',function(){
+       $('h2').addClass('clicked');
+    
+      
+    });
+    
+    jquery(window).load(function(){
+
+    });
+  
+}(jQuery));
